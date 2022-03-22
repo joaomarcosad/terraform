4f1a81677649d5df870fb8c7216ee147ff0891e3
João Marcos de A. Dantas <joaomarcos.ad@gmail.com>
2022-03-22 14:21:52 -0300

    alterando ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = var.enable_sg ? aws_security_group.optional[*].id : [data.aws_security_group.default.id]
+
+  tags = {
+    Name = var.name
+  }
+}
